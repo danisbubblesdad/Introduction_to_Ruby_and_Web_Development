f60ea647075734262cb824edd463cec28dd64087
Daniel Crittenden <dan.crittenden@gmail.com>
2016-05-04 19:04:25 -0400

    Adjusted calculator_method.rb per user feedback. Additional methods have been created to shorten the calculator method to a more reasonable line length. Eval has been replaced by a more sound string testing procedure. The output logic has been consolidated to limit the number of lines.

--- Lesson_1/calculator_method.rb
+++ Lesson_1/calculator_method.rb
@@ -1,22 +1,23 @@
-def test_number_input(input)
-  if input[0] == "."
-    input = "0" + input
+def request_user_data(numbers_array, operator)
+  say "Welcome to the world's finest archaic terminal calculator."
+  say "Please enter your first number:"
+  add_user_response_to_array(numbers_array)
+  say "Please enter your second number:"
+  add_user_response_to_array(numbers_array)
+
+  operators = ["+","-","*","/"]
+
+  while operators.include?(operator) == false
+    say "Please enter a valid operator (+, -, *, /)"
+    operator.replace(gets.chomp)
   end
-  klass = eval(input).class
-    return true
-  rescue NameError
-    say "**Entry is not a valid number. Try again.**"
-    return false
-  rescue SyntaxError
-    say "**Entry is not a valid number. Try again.**"
-    return false
 end
 
 def say(text)
   puts "=> #{text}"
 end
 
-def push_number_to_array(numbers)
+def add_user_response_to_array(numbers)
   loop do
     input = gets.chomp
     if test_number_input(input) == true
@@ -26,53 +27,54 @@ def push_number_to_array(numbers)
   end
 end
 
-def calculator
-  numbers = []
-
-  say "Welcome to the world's finest archaic terminal calculator."
-  say "Please enter your first number:"
-  push_number_to_array(numbers)
-  say "Please enter your second number:"
-  push_number_to_array(numbers)
-
-  operators = ["+","-","*","/"]
-  operator = ""
-  input = ""
-
-  while operators.include?(operator) == false
-    say "Please enter a valid operator (+, -, *, /)"
-    operator = gets.chomp
+def test_number_input(input)
+  if input[0] == "."
+    input = "0" + input
   end
+  if input.to_i.to_s == input || input.to_f.to_s == input
+    true
+  else
+    say "**Entry is not a valid number. Try again.**"
+    false
+  end
+end
 
-  num1 = numbers[0]
-  num2 = numbers[1]
+def convert_strings_to_numbers(array)
+  num1 = array.shift
+  num2 = array.shift
 
   if num1.to_i.to_s != num1 || num2.to_i.to_s != num2
-    num1 = num1.to_f
-    num2 = num2.to_f
+    array.push(num1.to_f)
+    array.push(num2.to_f)
   else
-    num1 = num1.to_i
-    num2 = num2.to_i
+    array.push(num1.to_i)
+    array.push(num2.to_i)
   end
+end
 
-  if operator == "+"
-    output = num1 + num2
-  elsif operator == "-"
-    output = num1 - num2
-  elsif operator == "*"
-    output = num1 * num2
-  elsif operator == "/"
-    if num1 % num2 != 0
-      output = num1.to_f / num2.to_f
-    else
-      output = num1 / num2
-    end
-  end
+def perform_calculations(numbers_array, operator, output)
+  output[0] =  if operator == "+"
+              numbers_array[0] + numbers_array[1]
+            elsif operator == "-"
+              numbers_array[0] - numbers_array[1]
+            elsif operator == "*"
+              numbers_array[0] * numbers_array[1]
+            elsif operator == "/"
+              if numbers_array[0] % numbers_array[1] == 0
+                numbers_array[0] / numbers_array[1]
+              else
+                numbers_array[0].to_f / numbers_array[1].to_f
+              end
+            end
+end
 
+def display_results_of_calculation(output)
   say "*Beep* *boop* *beep* *boop*."
-  say "The calculated value is #{output}."
+  say "The calculated value is #{output[0]}."
   puts "----------------------------------"
+end
 
+def ask_to_restart_calculator
   loop do
     say "Would you like to use the calculator again? 'Y' or 'N'?"
     input = gets.chomp.upcase
@@ -80,10 +82,21 @@ def calculator
       calculator
       break
     elsif input == "N"
-      say "Thank you for choosing the world's finest archaic terminal calculator for your mathmatical needs."
+      say "Thank you for choosing the world's finest archaic terminal calculator for your mathematical needs."
       break
     end
   end
+end
+
+def calculator
+  numbers_array = []
+  operator = ""
+  output = []
+  request_user_data(numbers_array, operator)
+  convert_strings_to_numbers(numbers_array)
+  perform_calculations(numbers_array, operator, output)
+  display_results_of_calculation(output)
+  ask_to_restart_calculator
 end
 
 calculator
