ab95707299f409256ee77b9ff249aee89a8ac92b
Daniel Crittenden <dan.crittenden@gmail.com>
2016-05-08 16:45:19 -0400

    Upload 'paper, rock, scissors' app for review

---
+++ Lesson_1/prs.rb
@@ -0,0 +1,38 @@
+CHOICES = {'p' => "paper", 'r' => "rock", 's' => "scissors"}
+game_hands = {}
+winning_hands = [["p","r"], ["r","s"], ["s", "p"]]
+
+puts "Paper, Rock, Scissors: THE GAME. V 0.0001"
+puts "An action-packed game fit for all species with fingers and access to a computer terminal."
+
+loop do
+
+  begin
+    puts "P, R, or S?"
+    game_hands[:user_hand] = gets.chomp.downcase
+  end until CHOICES.keys.include?(game_hands[:user_hand])
+
+  game_hands[:computer_hand] = CHOICES.keys.sample
+
+  puts %x{clear}
+  puts "::As you show #{CHOICES[game_hands[:user_hand]]}, you see that the computer chose #{CHOICES[game_hands[:computer_hand]]}::"
+  puts ""
+  puts ""
+
+  if game_hands[:user_hand] == game_hands[:computer_hand]
+    puts "TIE! You both chose #{CHOICES[game_hands[:user_hand]]}"
+  elsif winning_hands.include?(game_hands.values)
+    puts "You win! #{CHOICES[game_hands[:user_hand]].capitalize} beats #{CHOICES[game_hands[:computer_hand]]}!!"
+  else
+    puts "You lose! #{CHOICES[game_hands[:computer_hand]].capitalize} beats #{CHOICES[game_hands[:user_hand]]}.. :("
+  end
+
+  puts ""
+  puts ""
+  puts "Play again? (y/n)"
+  break if gets.chomp.downcase != 'y'
+  puts %x{clear}
+
+end
+
+puts "Thank you for playing!"
